3cb67654a8763da40644158c75aff302e725ab51
andygeiss <4ndygeiss@gmail.com>
2019-09-02 11:13:24 +0200

    ddd updated

--- 4-ddd/app.js
+++ 4-ddd/app.js
@@ -2,6 +2,10 @@ let repository = new Repository();
 let service = new Service(repository);
 let command = new Command(service, repository);
 
+// Events:
+// =======
+
+// Event "Name was updated" should trigger Command "render Name"
 command.bind("keyup", "txtInput", function (service, repository) {
     let name = document.getElementById("txtInput").value;
     repository.set("name", name);
